6e901bd0163b958b536c3e02f89dbdafe82def2e
Hermez <angelo_stk06@hotmail.com>
2023-05-16 13:35:38 -0500

    feat: add model tasks - see note

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Tasks(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    done = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.title
+
