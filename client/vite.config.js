9943e70e66b39e76ef964af75450936a3e8a98ff
Hermez <angelo_stk06@hotmail.com>
2023-05-24 14:54:38 -0500

    feat: final frontend

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/static/"
+  base: process.env.NODE_ENV === 'production' ? '/static/' : '/',
 })
